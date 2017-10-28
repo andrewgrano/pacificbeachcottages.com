4760bc283e034ff8ee99da26b4ba5c85fa09f4c0
andrewgrano <andrewgrano@gmail.com>
2017-10-27 18:02:57 -0700

    various edits per clients request

---
+++ assets/js/functions.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        items:1,
+        navText:['<img src="/img/arrow-left.svg" width="25" height="25">','<img src="/img/arrow-right.svg" width="25" height="25">']
+    })
+});
