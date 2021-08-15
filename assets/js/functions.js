e28cb96f7e953dbad998706f3aae3e93e6a59518
Andrew Grano <andrewgrano@Andrews-MacBook-Pro.local>
2021-08-15 14:41:49 -0700

    update js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,6 +3,6 @@ $(function() {
         loop:true,
         nav:true,
         items:1,
-        navText:['<img src="./assets/img/arrow-left.svg" width="25" height="25">','<img src="./assets/img/arrow-right.svg" width="25" height="25">']
+        navText:['<img src="../assets/img/arrow-left.svg" width="25" height="25">','<img src="../assets/img/arrow-right.svg" width="25" height="25">']
     })
 });
